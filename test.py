e6bab6b1fa07a12467f449a47871047addd79f94
singhanat <singhanat>
2024-10-30 04:22:03 +0000

    แก้ไข pipeline และ test

--- test.py
+++ test.py
@@ -1,33 +1,44 @@
 # Databricks notebook source
 import pandas as pd
+import configparser
+import os
 from datetime import datetime
 
-# file path
-inputPath = "/Workspace/Users/singhanat.rer@kmutt.ac.th/track_small.csv"
-outputPath = "/Workspace/Users/singhanat.rer@kmutt.ac.th/output_small.csv"
-testResultPath = "/Workspace/Users/singhanat.rer@kmutt.ac.th/test_result.txt"
+# Get the parent path of the notebook
+notebook_path = dbutils.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+parent_path = os.path.dirname('/Workspace' + notebook_path)
+os.chdir(parent_path)
 
-# open test result file
-f = open(testResultPath, "w")
+# read config file
+config = configparser.ConfigParser()
+config.read('./pipeline.conf')
+inputPath = config.get('DEFAULT', 'INPUT_PATH')
+outputPath = config.get('DEFAULT', 'OUTPUT_PATH')
+testResultPath = config.get('DEFAULT', 'TEST_RESULT_PATH')
 
-# read csv file
+# read files
 tracksInput = pd.read_csv(inputPath)
 tracksOutput = pd.read_csv(outputPath)
 
+# open test result file
+f = open(testResultPath, "a")
+
+# write datetime
+f.write(datetime.now().strftime("%d-%m-%Y %H:%M:%S") + '\n')
+
 # Case 1
- 
-if tracksOutput.dtypes["UnitPrice"] == 'int64':
-    f.write("Case 1: Passed\n")
+unitPriceType = tracksOutput.dtypes['UnitPrice']
+if unitPriceType == 'int64':
+    f.write("Case 1: Pass\n")
 else:
-    f.write("Case 1: Failed\n")
+    f.write("Case 1: Fail\n")
 
 # Case 2
-
-mergedTracks = tracksInput.merge(tracksOutput, on="TrackId", suffixes=("_input", "_output"))
-if (mergedTracks["UnitPrice_output"] - mergedTracks["UnitPrice_input"] < 1).all():
-    f.write("Case 2: Passed\n")
+mergedTracks = pd.merge(tracksInput, tracksOutput, on='TrackId', suffixes=('_input', '_output'))
+if (mergedTracks['UnitPrice_output'] - mergedTracks['UnitPrice_input'] < 1).all():
+    f.write("Case 2: Pass\n")
 else:
-    f.write("Case 2: Failed\n")
+    f.write("Case 2: Fail\n")
 
-# close
+# close test result file
 f.close()
